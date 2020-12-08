9090b712db08f9d57a6086b303843a7a01018da6
viits <toyobr@hotmail.com>
2020-12-08 19:33:07 -0300

    Classe vedendor

--- src/application/Application.java
+++ src/application/Application.java
@@ -1,9 +1,11 @@
 package application;
 
 import java.sql.Connection;
+import java.util.Date;
 
 import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Application {
 
@@ -11,7 +13,11 @@ public class Application {
 		Connection conn = DB.getConnection();
 		
 		Department obj = new Department(1,"books");
-		System.out.println(obj);
+	
+		Seller seller = new Seller(21,"Bob", "bob@gmail.com", new Date(),3000.0,obj);
+		
+		System.out.println(seller);
+		
 		DB.closeConnection();
 	}
 

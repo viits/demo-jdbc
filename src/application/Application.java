95b619ccc1d63800006153ea57a2e8d488a86192
viits <toyobr@hotmail.com>
2020-12-08 19:19:06 -0300

    Classe departamento

--- src/application/Application.java
+++ src/application/Application.java
@@ -3,14 +3,15 @@ package application;
 import java.sql.Connection;
 
 import db.DB;
+import model.entities.Department;
 
 public class Application {
 
 	public static void main(String[] args) {
 		Connection conn = DB.getConnection();
 		
-		System.out.println("Hello world");
-		System.out.println("My name is Esdras.");
+		Department obj = new Department(1,"books");
+		System.out.println(obj);
 		DB.closeConnection();
 	}
 
